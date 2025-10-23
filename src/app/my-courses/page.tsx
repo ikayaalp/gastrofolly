cad600c1f795a67bc50afdf837e61f5345330560
ikayaalp <ismail@ergcontrols.net>
2025-10-23 20:15:05 +0300

    1

--- src/app/my-courses/page.tsx
+++ src/app/my-courses/page.tsx
@@ -294,10 +294,6 @@ export default function MyCoursesPage() {
             <MessageCircle className="h-6 w-6" />
             <span className="text-xs font-medium mt-1">Mesajlar</span>
           </Link>
-          <Link href="/contact" className="flex flex-col items-center py-2 px-3 text-gray-300 hover:text-white transition-colors">
-            <Phone className="h-6 w-6" />
-            <span className="text-xs font-medium mt-1">İletişim</span>
-          </Link>
         </div>
       </div>
 
