cc1f5a0dad33a2d1ed3d3d7b17f4a94b9e16f13b
ikayaalp <ismail@ergcontrols.net>
2026-01-30 22:25:44 +0300

    Loosen CSP to debug CORS issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ const securityHeaders = {
     'X-XSS-Protection': '1; mode=block',
     'Referrer-Policy': 'strict-origin-when-cross-origin',
     'Permissions-Policy': 'camera=(), microphone=(), geolocation=()',
-    'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://*.cloudinary.com https://res.cloudinary.com https://images.unsplash.com https://via.placeholder.com https://firebasestorage.googleapis.com https://lh3.googleusercontent.com; connect-src 'self' https://vitals.vercel-insights.com https://www.google-analytics.com https://*.cloudinary.com https://api.cloudinary.com; font-src 'self'; object-src 'none'; media-src 'self' blob: https://*.cloudinary.com https://res.cloudinary.com;",
+    'Content-Security-Policy': "default-src 'self' *; script-src 'self' 'unsafe-eval' 'unsafe-inline' *; style-src 'self' 'unsafe-inline' *; img-src 'self' blob: data: *; connect-src 'self' *; font-src 'self' data: *; object-src 'none'; media-src 'self' blob: *;",
 }
 
 export function middleware(request: NextRequest) {
