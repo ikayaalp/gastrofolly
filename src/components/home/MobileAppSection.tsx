34b3862a3aae92c143cfd18c0339e2404fd2cc44
ikayaalp <ismail@ergcontrols.net>
2026-03-01 19:52:50 +0300

    Match App Store and Google Play button sizes to 200x56px

--- src/components/home/MobileAppSection.tsx
+++ src/components/home/MobileAppSection.tsx
@@ -74,7 +74,7 @@ export default function MobileAppSection() {
 
                         <div className="flex flex-col sm:flex-row items-center gap-4">
                             {/* App Store Button */}
-                            <button className="flex items-center gap-3 bg-black border border-[#A6A6A6] rounded-lg px-4 py-2.5 hover:bg-white/5 transition-all w-full sm:w-auto">
+                            <button className="flex items-center gap-3 bg-black border border-[#A6A6A6] rounded-lg px-5 h-[56px] hover:bg-white/5 transition-all w-[200px]">
                                 <svg viewBox="0 0 384 512" className="w-7 h-7 fill-white flex-shrink-0">
                                     <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
                                 </svg>
@@ -85,8 +85,8 @@ export default function MobileAppSection() {
                             </button>
 
                             {/* Google Play Button - Official Google Badge */}
-                            <a href="#" className="block w-full sm:w-auto transition-transform hover:scale-105 duration-300">
-                                <img src="/assets/google-play-badge.png" alt="Google Play'den Hemen İndirin" className="h-[52px] w-auto object-contain" />
+                            <a href="#" className="block transition-transform hover:scale-105 duration-300 w-[200px]">
+                                <img src="/assets/google-play-badge.png" alt="Google Play'den Hemen İndirin" className="h-[56px] w-[200px] object-contain" />
                             </a>
                         </div>
                     </motion.div>
